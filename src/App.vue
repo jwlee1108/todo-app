ac0c77008b66ab11adc3e2c85e6791cf6b6e9a97
jinwoo-lee <jw.lee@nhn.com>
2023-02-13 00:13:52 +0900

    01-02. 리스트 렌더링

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <ul>
-    <li>{{ todo.title }}</li>
+    <li v-for="(todo, index) in todos" :key="index">
+      {{ todo.title }}
+    </li>
   </ul>
 </template>
 <script>
 export default {
   data() {
     return {
-      todo: {
-        title: 'Learning Vue!'
-      }
+      todos: [
+        { title: 'something to do' },
+        { title: 'what I have to do' },
+      ]
     }
   }
 }
