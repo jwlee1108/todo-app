9319d002a669bb1e18a116c14c7df85bc5cc7739
jinwoo-lee <jw.lee@nhn.com>
2023-02-13 23:40:10 +0900

    01-04: 체크박스 이벤트 연결

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <ul>
-    <li v-for="(todo, index) in todos" :key="index">
+    <li v-for="(todo, index) in todos" :key="index"  @click="changeComplete(todo)">
       <input type="checkbox" :checked="todo.complete"/>{{ todo.title }}
     </li>
   </ul>
@@ -14,8 +14,17 @@ export default {
         { title: 'what I have to do', complete: false },
       ]
     }
+  },
+  methods: {
+    changeComplete(todo) {
+      todo.complete = !todo.complete;
+      console.log(`todo: ${todo.title} is ${todo.complete ? 'complete' : 'incomplete'}`);
+    }
   }
 }
 </script>
 <style>
+li {
+  user-select: none;
+}
 </style>
