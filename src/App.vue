304586647bdcca119fecd46af781c02f11371113
jinwoo-lee <jw.lee@nhn.com>
2023-02-13 22:51:21 +0900

    exercise: 인라인 메소드 이벤트 핸들러

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
-  <p>{{ message }}</p>
+  <div>
+    <button v-on:click="count('Increment')">Add 1</button>
+    <button v-on:click="count('Decrement')">Sub 1</button>
+    <p>counter: {{ counter }}</p>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      message: 'Hello Vue!'
+      counter: 0
+    }
+  },
+  methods: {
+    count(action) {
+      if (action === 'Increment') {
+        this.counter += 1;
+      } else if (action === 'Decrement') {
+        this.counter -= 1;
+      }
     }
   }
 }
