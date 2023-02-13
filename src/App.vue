8409d8f281be5d80e330706aaf236d4953a1aeac
jinwoo-lee <jw.lee@nhn.com>
2023-02-13 22:26:55 +0900

    exercise: 이벤트 핸들링 - 자바스크립트 표현식

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <p>{{ message }}</p>
+  <div id="counter">
+    <button @click="count += 1">Add 1</button>
+    <p>You clicked {{ count }} times.</p>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      message: 'Hello Vue!'
+      count: 0
     }
   }
 }
