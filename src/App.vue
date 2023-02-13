e72195811d1083e9865548ff0d34973b7aba3332
jinwoo-lee <jw.lee@nhn.com>
2023-02-14 00:13:02 +0900

    01-05. 새로운 할 일 추가

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <ul>
-    <li v-for="(todo, index) in todos" :key="index"  @click="changeComplete(todo)">
-      <input type="checkbox" :checked="todo.complete"/>{{ todo.title }}
-    </li>
-  </ul>
+  <div>
+    <div>
+      <input type="text" v-model="newTodo" @keyup.enter="addTodo" />
+      <button @click="addTodo">Add</button>
+    </div>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index"  @click="changeComplete(todo)">
+        <input type="checkbox" :checked="todo.complete"/>{{ todo.title }}
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {
+      newTodo: '',
       todos: [
         { title: 'something to do', complete: true },
         { title: 'what I have to do', complete: false },
@@ -19,6 +26,10 @@ export default {
     changeComplete(todo) {
       todo.complete = !todo.complete;
       console.log(`todo: ${todo.title} is ${todo.complete ? 'complete' : 'incomplete'}`);
+    },
+    addTodo() {
+      this.todos.push({ title: this.newTodo, complete: false });
+      this.newTodo = '';
     }
   }
 }
