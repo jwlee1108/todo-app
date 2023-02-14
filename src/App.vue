3a370e6ddf76164670c908318a5c5fb017535a4d
jinwoo-lee <jw.lee@nhn.com>
2023-02-14 23:41:26 +0900

    exercise: computed 반응형 데이터

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
-  <p>{{ message }}</p>
+  <div>
+    <button @click="addLeftOperand">Add num1</button>
+    <button @click="addRightOperand">Add num2</button>
+    <p>{{ sum }}</p>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      message: 'Hello Vue!'
+      leftOperand: 0,
+      rightOperand: 0
+    }
+  },
+  computed: {
+    sum() {
+      return this.leftOperand + this.rightOperand;
+    }
+  },
+  methods: {
+    addLeftOperand() {
+      this.leftOperand += 1;
+      console.log(`leftOperand: ${this.leftOperand}`);
+    },
+    addRightOperand() {
+      this.rightOperand += 1;
+      console.log(`rightOperand: ${this.rightOperand}`);
     }
   }
 }
