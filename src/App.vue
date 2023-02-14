5e34a0c9112c85df1cd51e7b24a664274ac24748
jinwoo-lee <jw.lee@nhn.com>
2023-02-14 23:51:58 +0900

    exercise: computed getter/setter

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <template>
-  <p>{{ message }}</p>
+  <div>
+    <button @click="shuffleFullName">Shuffle</button>
+    <p>{{ fullName }}</p>
+  </div>
 </template>
 <script>
+const NAMES = [
+  'John Doe',
+  'Jane Doe',
+  'John Smith',
+  'Jane Smith'
+];
 export default {
   data() {
     return {
-      message: 'Hello Vue!'
+      firstName: 'John',
+      lastName: 'Doe'
+    }
+  },
+  computed: {
+    fullName: {
+      get() {
+        return `${this.firstName} ${this.lastName}`;
+      },
+      set(newName) {
+        const names = newName.split(' ');
+        this.firstName = names[0];
+        this.lastName = names[names.length - 1];
+      }
+    }
+  },
+  methods: {
+    shuffleFullName() {
+      this.fullName = NAMES[Math.floor(Math.random() * NAMES.length)];
     }
   }
 }
