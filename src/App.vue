0b9f9876c276667f596e79d18dec5fec5fb75faa
jinwoo-lee <jw.lee@nhn.com>
2023-02-15 22:26:35 +0900

    Revert "exercise: computed getter/setter"
    This reverts commit 5e34a0c9112c85df1cd51e7b24a664274ac24748.

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,11 @@
 <template>
-  <div>
-    <button @click="shuffleFullName">Shuffle</button>
-    <p>{{ fullName }}</p>
-  </div>
+  <p>{{ message }}</p>
 </template>
 <script>
-const NAMES = [
-  'John Doe',
-  'Jane Doe',
-  'John Smith',
-  'Jane Smith'
-];
 export default {
   data() {
     return {
-      firstName: 'John',
-      lastName: 'Doe'
-    }
-  },
-  computed: {
-    fullName: {
-      get() {
-        return `${this.firstName} ${this.lastName}`;
-      },
-      set(newName) {
-        const names = newName.split(' ');
-        this.firstName = names[0];
-        this.lastName = names[names.length - 1];
-      }
-    }
-  },
-  methods: {
-    shuffleFullName() {
-      this.fullName = NAMES[Math.floor(Math.random() * NAMES.length)];
+      message: 'Hello Vue!'
     }
   }
 }
